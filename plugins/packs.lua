8aa11943388039bb200159526b38ef417284d753
Zachery Hostens <zachery@textus.com>
2023-04-06 20:48:36 -0500

    add astro community packages

---
+++ plugins/packs.lua
@@ -0,0 +1,11 @@
+return {
+  "AstroNvim/astrocommunity",
+  { import = "astrocommunity.colorscheme.tokyonight" },
+  { import = "astrocommunity.pack.bash" },
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.pack.nix" },
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.yaml" },
+}

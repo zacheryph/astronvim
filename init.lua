e9f6e959a56daf46df31b10878c57497876a799a
Zachery Hostens <zachery@textus.com>
2023-04-06 20:51:05 -0500

    Change colorscheme to tokyonight-storm

--- init.lua
+++ init.lua
@@ -18,7 +18,7 @@ return {
   },
 
   -- Set colorscheme to use
-  colorscheme = "astrodark",
+  colorscheme = "tokyonight-storm",
 
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {
